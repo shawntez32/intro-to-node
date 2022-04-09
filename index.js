ae23f39d179d2526ed0290bc7859ee5360491d89
Shawntez Malone <69469670+shawntez32@users.noreply.github.com>
2022-04-09 16:56:51 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var hero = superheroes.random();
+var villain = supervillains.random();
+console.log(hero);
+console.log(villain);
